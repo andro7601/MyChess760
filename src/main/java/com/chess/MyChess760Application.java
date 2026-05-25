9435b34bad9c6579a29ec934bec40bf1e704b229
Andria Shvelidze <shvelidze2007@gmail.com>
2026-05-25 18:07:41 +0400

    initial commit

---
+++ src/main/java/com/chess/MyChess760Application.java
@@ -0,0 +1,13 @@
+package com.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChess760Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChess760Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chess/MyChess760ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChess760ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
