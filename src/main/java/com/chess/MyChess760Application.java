f2743365d8acdf2507de7636235a36784c33f1cb
Andria Shvelidze <shvelidze2007@gmail.com>
2026-05-25 21:47:10 +0400

    composed containers everythings running

--- src/main/java/com/chess/MyChess760Application.java
+++ src/main/java/com/chess/MyChess760Application.java
@@ -1,13 +1,17 @@
 package com.chess;
 
 import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MyChess760Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyChess760Application.class, args);
+		log.debug("i am Spiderman");
+
 	}
 
 }
