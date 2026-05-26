8780371ccac019a6b14d1c0a005a66e7ea7c66db
Andria Shvelidze <shvelidze2007@gmail.com>
2026-05-26 20:18:00 +0400

    added websocket and redis config

---
+++ src/main/java/com/chess/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package com.chess.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.setApplicationDestinationPrefixes("/app");
+
+        registry.enableSimpleBroker("/sub");
+
+        registry.setUserDestinationPrefix("/user");
+        //Prefix for private messages
+        //Server to Client automatically configures the user from principal.getName()
+
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();//front i guess
+    }
+}
